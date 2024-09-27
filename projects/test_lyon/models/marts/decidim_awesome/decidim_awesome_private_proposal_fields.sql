a27d612ac832386581bffebf12cb85e70917a35f
fujistone <fujistone@hotmail.fr>
2024-09-27 14:43:27 +0200

    adding extra fields

--- projects/test_lyon/models/marts/decidim_awesome/decidim_awesome_private_proposal_fields.sql
+++ projects/test_lyon/models/marts/decidim_awesome/decidim_awesome_private_proposal_fields.sql
@@ -1,11 +1,11 @@
 SELECT
-    decidim_awesome_private_proposal_fields.id,
-    decidim_awesome_private_proposal_fields.proposal_id,
-    decidim_awesome_private_proposal_fields.private_field_description,
-    decidim_awesome_private_proposal_fields.private_field_content,
+    decidim_awesome_proposal_extra_fields.id,
+    decidim_awesome_proposal_extra_fields.proposal_id,
+    decidim_awesome_proposal_extra_fields._field_description,
+    decidim_awesome_proposal_extra_fields._field_content,
     decidim_proposals_proposals.decidim_component_id,
     components.ps_title
 FROM
-    {{ ref ("int_decidim_awesome_private_proposal_fields")}} AS decidim_awesome_private_proposal_fields
-JOIN decidim_proposals_proposals ON decidim_awesome_private_proposal_fields.proposal_id = decidim_proposals_proposals.id
+    {{ ref ("int_decidim_awesome_proposal_extra_fields")}} AS decidim_awesome__proposal_extra_fields
+JOIN decidim_proposals_proposals ON decidim_awesome_proposal_extra_fields.proposal_id = decidim_proposals_proposals.id
 JOIN {{ ref ("components")}} AS components ON decidim_proposals_proposals.decidim_component_id = components.id
---
+++ projects/test_lyon/models/staging/decidim/stg_decidim_awesome_proposal_extra_fields.sql
@@ -0,0 +1,11 @@
+WITH source AS (
+      SELECT * FROM {{ source('decidim', 'decidim_awesome_proposal_extra_fields') }}
+)
+
+SELECT
+    id,
+    decidim_proposal_id AS proposal_id,
+    updated_at,
+    created_at,
+    private_body
+FROM source
