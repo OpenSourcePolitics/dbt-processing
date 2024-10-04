3ce03f8a63369ed3e43bd754371e4c1d333750e6
fujistone <fujistone@hotmail.fr>
2024-10-04 08:45:03 +0200

    deleting useless models 2 folder

--- projects/lyon/models/marts/decidim_awesome/decidim_awesome_proposal_extra_fields.sql
+++ projects/lyon/models/marts/decidim_awesome/decidim_awesome_proposal_extra_fields.sql
@@ -1,11 +1,18 @@
 SELECT
     decidim_awesome_proposal_extra_fields.id,
     decidim_awesome_proposal_extra_fields.proposal_id,
-    decidim_awesome_proposal_extra_fields.private_field_description,
-    decidim_awesome_proposal_extra_fields.private_field_content,
-    decidim_proposals_proposals.decidim_component_id,
-    components.ps_title
+    MAX(CASE 
+        WHEN decidim_awesome_proposal_extra_fields.private_field_description = 'Cette idée est déposée à titre :' 
+        THEN decidim_awesome_proposal_extra_fields.private_field_content 
+        END) AS description_content,
+    MAX(CASE 
+        WHEN decidim_awesome_proposal_extra_fields.private_field_description = 'Votre tranche d age :' 
+        THEN decidim_awesome_proposal_extra_fields.private_field_content 
+        END) AS age_range_content
 FROM
-    {{ ref ("int_decidim_awesome_proposal_extra_fields")}} AS decidim_awesome_proposal_extra_fields
-JOIN {{ ref ("stg_decidim_proposals")}} AS decidim_proposals_proposals ON decidim_awesome_proposal_extra_fields.proposal_id = decidim_proposals_proposals.id
-JOIN {{ ref ("components")}} AS components ON decidim_proposals_proposals.decidim_component_id = components.id
+    {{ ref("int_decidim_awesome_proposal_extra_fields") }} AS decidim_awesome_proposal_extra_fields
+JOIN {{ ref("stg_decidim_proposals") }} AS decidim_proposals ON 
+    decidim_awesome_proposal_extra_fields.proposal_id = decidim_proposals.id
+GROUP BY 
+    decidim_awesome_proposal_extra_fields.id,
+    decidim_awesome_proposal_extra_fields.proposal_id
