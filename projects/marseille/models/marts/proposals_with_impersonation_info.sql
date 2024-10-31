a3aa09ec25ac5a5ed86a376303c0b64593193879
ailepet <hellpe@users.noreply.github.com>
2024-10-31 15:03:15 +0100

    add users with promote status model

--- projects/marseille/models/marts/proposals_with_impersonation_info.sql
+++ projects/marseille/models/marts/proposals_with_impersonation_info.sql
@@ -1,8 +1,8 @@
 WITH proposals_under_impersonation AS (
     SELECT 
-    proposals.id AS proposal_id
-    FROM {{ ref("proposals") }} AS proposals
-    LEFT JOIN {{ ref("decidim_impersonation_logs") }} AS impersonations ON proposals.first_author_id = impersonations.user_id
+        proposals.id AS proposal_id
+    FROM {{ ref('proposals') }} AS proposals
+    LEFT JOIN {{ ref('stg_decidim_impersonation_logs') }} AS impersonations ON proposals.first_author_id = impersonations.user_id
     WHERE proposals.created_at BETWEEN impersonations.started_at AND impersonations.ended_at
     ORDER BY proposals.id
 )
@@ -10,5 +10,5 @@ WITH proposals_under_impersonation AS (
 SELECT
     *,
     (CASE WHEN proposals_under_impersonation.proposal_id IS NOT NULL THEN true ELSE false END) AS authored_under_impersonation
-FROM {{ ref("proposals") }} AS proposals
+FROM {{ ref('proposals') }} AS proposals
 LEFT JOIN proposals_under_impersonation ON proposals.id = proposals_under_impersonation.proposal_id
