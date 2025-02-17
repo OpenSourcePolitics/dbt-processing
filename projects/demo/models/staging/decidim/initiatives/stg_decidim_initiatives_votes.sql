0f95db2d9b20beba77619041790d6ab21da95f23
fujistone <fujistone@hotmail.fr>
2025-02-17 17:06:40 +0100

    feat(initiative_votes) : changing author_id to user_id in staging and marts models

--- projects/demo/models/staging/decidim/initiatives/stg_decidim_initiatives_votes.sql
+++ projects/demo/models/staging/decidim/initiatives/stg_decidim_initiatives_votes.sql
@@ -8,7 +8,7 @@
     SELECT
         id,
         decidim_initiative_id,
-        decidim_author_id,
+        decidim_author_id as user_id,
         created_at,
         updated_at,
         encrypted_metadata,
@@ -20,7 +20,7 @@
     SELECT
         CAST(NULL AS INTEGER) AS id,
         CAST(NULL AS INTEGER) AS decidim_initiative_id,
-        CAST(NULL AS INTEGER) AS decidim_author_id,
+        CAST(NULL AS INTEGER) AS user_id,
         CAST(NULL AS TIMESTAMP) AS created_at,
         CAST(NULL AS TIMESTAMP) AS updated_at,
         CAST(NULL AS TEXT) AS encrypted_metadata,
