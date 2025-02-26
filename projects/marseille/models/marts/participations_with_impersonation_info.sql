e53db44247f34e48e91195a6f89ea1ab8960e776
ailepet <hellpe@users.noreply.github.com>
2025-02-26 15:51:58 +0100

    fix(marseille): fix join with impersonation logs

--- projects/marseille/models/marts/participations_with_impersonation_info.sql
+++ projects/marseille/models/marts/participations_with_impersonation_info.sql
@@ -1,8 +1,9 @@
 WITH participations_under_impersonation AS (
     SELECT
-       participations.participation_id AS impersonated_participation_id
+        participations.participation_id AS impersonated_participation_id,
+        participations.participation_type AS impersonated_participation_type
     FROM {{ ref('participations') }} as participations
-    LEFT JOIN {{ ref('stg_decidim_impersonation_logs') }} AS impersonations ON participations.user_id = impersonations.user_id
+    JOIN {{ ref('stg_decidim_impersonation_logs') }} AS impersonations ON participations.user_id = impersonations.user_id
     WHERE participations.participation_date BETWEEN impersonations.started_at AND impersonations.ended_at
     ORDER BY participations.participation_id
 )
@@ -11,4 +12,6 @@ SELECT
     *,
     (CASE WHEN participations_under_impersonation.impersonated_participation_id IS NOT NULL THEN true ELSE false END) AS authored_under_impersonation
 FROM {{ ref('participations') }} AS participations
-LEFT JOIN participations_under_impersonation ON participations.participation_id = participations_under_impersonation.impersonated_participation_id
+LEFT JOIN participations_under_impersonation
+    ON participations.participation_id = participations_under_impersonation.impersonated_participation_id
+    AND participations.participation_type = impersonated_participation_type
