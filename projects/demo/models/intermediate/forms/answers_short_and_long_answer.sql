67ecd58eea49822c60e361af6fcf2c6bf3a0303d
ailepet <hellpe@users.noreply.github.com>
2025-05-15 16:23:55 +0200

    revert useless short_and_long_answer modification + rewrote test accordingly

--- projects/demo/models/intermediate/forms/answers_short_and_long_answer.sql
+++ projects/demo/models/intermediate/forms/answers_short_and_long_answer.sql
@@ -12,7 +12,7 @@ SELECT
     decidim_forms_questions.position,
     {{ int_forms_translate_short_and_long_answers('decidim_forms_answers.body') }} AS answer,
     '' AS sub_matrix_question,
-    COALESCE(decidim_forms_answer_choices.custom_body, '') AS custom_body,
+    '' AS custom_body,
     -1 AS sorting_position,
     decidim_forms_questions.decidim_questionnaire_id,
     decidim_forms_questions.body,
@@ -20,5 +20,4 @@ SELECT
     decidim_forms_answers.author_status
 FROM {{ ref('int_forms_answers') }} decidim_forms_answers
 JOIN {{ ref('stg_decidim_forms_questions') }} AS decidim_forms_questions ON decidim_forms_questions.id = decidim_forms_answers.decidim_question_id
-JOIN {{ ref('stg_decidim_forms_answer_choices') }} decidim_forms_answer_choices ON decidim_forms_answer_choices.decidim_answer_id = decidim_forms_answers.id
 WHERE question_type = ANY('{short_answer,long_answer}'::text[])
