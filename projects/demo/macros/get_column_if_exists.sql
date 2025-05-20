4b133dadbb8cb6adde600661ec939b4a2ea17605
ailepet <hellpe@users.noreply.github.com>
2025-05-20 10:45:46 +0200

    handle absence of phone_number column

--- projects/demo/macros/get_column_if_exists.sql
+++ projects/demo/macros/get_column_if_exists.sql
@@ -1,9 +1,14 @@
-{% macro get_column_if_exists(relation, column_name, cast_type="TEXT", default_value="NULL") %}
+{% macro get_column_if_exists(relation, column_name, cast_type="TEXT", default_value="NULL", include_alias=True) %}
   {% set columns = adapter.get_columns_in_relation(relation) %}
-  {% if column_name in columns | map(attribute="name") %}
+  {% set column_names = columns | map(attribute="name") | list %}
+
+  {% if column_name in column_names %}
     {{ column_name }}
   {% else %}
-    CAST({{ default_value }} AS {{ cast_type }}) AS {{ column_name }}
+    {% if include_alias %}
+      CAST({{ default_value }} AS {{ cast_type }}) AS {{ column_name }}
+    {% else %}
+      CAST({{ default_value }} AS {{ cast_type }})
+    {% endif %}
   {% endif %}
 {% endmacro %}
-
--- projects/demo/models/staging/decidim/users/stg_decidim_users.sql
+++ projects/demo/models/staging/decidim/users/stg_decidim_users.sql
@@ -41,7 +41,9 @@ renamed AS (
         NULLIF(extended_data::jsonb->>'gender', '') AS gender,
         NULLIF(extended_data::jsonb->>'postal_code', '') AS postal_code,
         NULLIF(extended_data::jsonb->>'half_signup', '') AS half_signup,
-        COALESCE(extended_data::jsonb->'half_signup'->>'phone_number', phone_number) AS half_signup_phone_number,
+        COALESCE(extended_data::jsonb->'half_signup'->>'phone_number',
+                   {{ get_column_if_exists(source('decidim', 'decidim_users'), 'phone_number', include_alias=False) }}
+                ) AS half_signup_phone_number,
         NULLIF(extended_data::jsonb->'half_signup'->>'phone_country', '') AS half_signup_phone_country,
         extended_data
     FROM source
