b45a649a44fa44e10519559efdfd566bd473866f
ailepet <hellpe@users.noreply.github.com>
2025-05-28 11:01:11 +0200

    fix: remove double quotes from answer choices in order to match answer options

--- projects/demo/models/intermediate/forms/answers_option_and_sorting.sql
+++ projects/demo/models/intermediate/forms/answers_option_and_sorting.sql
@@ -10,7 +10,7 @@ SELECT DISTINCT
     decidim_forms_answers.ip_hash,
     decidim_forms_questions.question_type,
     decidim_forms_questions.position AS question_position,
-    decidim_forms_answer_choices.body AS "answer",
+    decidim_forms_answer_choices.body::text AS answer,
     '' AS sub_matrix_question,
     COALESCE(decidim_forms_answer_choices.custom_body, '') AS custom_body,
     (CASE question_type WHEN 'sorting' THEN decidim_forms_answer_choices.position ELSE -1 END) AS sorting_position,
@@ -25,3 +25,4 @@ JOIN {{ ref('stg_decidim_forms_answer_options') }} decidim_forms_answer_options
   ON decidim_forms_answer_options.decidim_question_id = decidim_forms_questions.id
   AND decidim_forms_answer_options.body = decidim_forms_answer_choices.body
 WHERE question_type = ANY('{single_option, multiple_option, sorting}'::text[])
+ORDER BY decidim_questionnaire_id DESC
