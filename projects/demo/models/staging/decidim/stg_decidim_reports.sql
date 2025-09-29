26563d8847bb25a48469aaf69c754acda42f7cfb
ailepet <hellpe@users.noreply.github.com>
2025-09-29 11:56:09 +0200

    moved to marts + manage missing matomo columns on Club

---
+++ projects/demo/models/staging/decidim/stg_decidim_reports.sql
@@ -0,0 +1,29 @@
+{% set relation = adapter.get_relation(
+    database=target.database,
+    schema='public',
+    identifier='decidim_reports'
+) %}
+
+{% if relation is not none %}
+SELECT
+    id,
+    decidim_moderation_id,
+    decidim_user_id,
+    reason,
+    details,
+    created_at,
+    updated_at,
+    locale
+FROM {{ source('decidim', 'decidim_reports') }}
+{% else %}
+    SELECT
+    	CAST(NULL AS INTEGER) AS id,
+    	CAST(NULL AS INTEGER) AS decidim_moderation_id,
+    	CAST(NULL AS INTEGER) AS decidim_user_id,
+    	CAST(NULL AS TEXT) AS reason,
+		CAST(NULL AS TEXT) AS details,
+		CAST(NULL AS TIMESTAMP) AS created_at,
+		CAST(NULL AS TIMESTAMP) AS updated_at,
+		CAST(NULL AS TEXT) AS locale
+    LIMIT 0
+{% endif %}
--- projects/demo/models/staging/matomo/stg_matomo_hourly_visits.sql
+++ projects/demo/models/staging/matomo/stg_matomo_hourly_visits.sql
@@ -1,3 +1,10 @@
+{% set relation = adapter.get_relation(
+    database=target.database,
+    schema='matomo',
+    identifier='hourly_visits'
+) %}
+
+{% if relation is not none %}
 SELECT
     label,
     nb_uniq_visitors,
@@ -12,3 +19,19 @@ SELECT
     segment,
     date
 FROM {{ source('matomo', 'hourly_visits') }}
+{% else %}
+    SELECT
+        CAST(NULL AS TEXT) AS label,
+        CAST(NULL AS INTEGER) AS nb_uniq_visitors,
+        CAST(NULL AS INTEGER) AS nb_visits,
+        CAST(NULL AS INTEGER) AS nb_actions,
+        CAST(NULL AS INTEGER) AS nb_users,
+        CAST(NULL AS INTEGER) AS max_actions,
+        CAST(NULL AS INTEGER) AS sum_visit_length,
+        CAST(NULL AS INTEGER) AS bounce_count,
+        CAST(NULL AS INTEGER) AS nb_visits_converted,
+        CAST(NULL AS TIMESTAMP) AS visittime_localtime,
+        CAST(NULL AS TEXT) AS segment,
+        CAST(NULL AS TIMESTAMP) AS date
+    LIMIT 0
+{% endif %}
