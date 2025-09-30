227229f4869b48a386bbb95df4f8c59fee7536d0
ailepet <hellpe@users.noreply.github.com>
2025-09-30 17:14:11 +0200

    remove failing constraint

--- projects/cour_des_comptes/models/marts/participations_with_extended_data.sql
+++ projects/cour_des_comptes/models/marts/participations_with_extended_data.sql
@@ -2,6 +2,7 @@ SELECT
     DISTINCT ON (user_id, ps_title)
     user_id,
     users.age_category AS age_category,
+    users.gender AS gender,
     decidim_component_id,
     components.ps_title AS ps_title
 FROM {{ ref("participations")}} participations
