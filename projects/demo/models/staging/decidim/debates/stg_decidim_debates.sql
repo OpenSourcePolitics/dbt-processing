81f285a1511fc7f6b5e7eec813b095af50ca8e54
ailepet <hellpe@users.noreply.github.com>
2026-01-20 16:47:21 +0100

    fix coalesce_legacy_and_new_columns macro

--- projects/demo/models/staging/decidim/debates/stg_decidim_debates.sql
+++ projects/demo/models/staging/decidim/debates/stg_decidim_debates.sql
@@ -14,7 +14,7 @@ renamed AS (
         decidim_author_id,
         created_at,
         closed_at,
-        {{ coalesce_legacy_and_new_columns(source('decidim', 'decidim_debates_debates'), 'endorsements_count', 'likes_count') }},
+        {{ coalesce_legacy_and_new_columns(source('decidim', 'decidim_debates_debates'), 'endorsements_count', 'likes_count', 'INTEGER') }},
         'Decidim::Debates::Debate' as resource_type
     FROM source 
 )
