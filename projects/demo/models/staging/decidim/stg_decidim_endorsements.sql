0adabd0a7b6a563f027c36b3568a1b4ebf74d80c
ailepet <hellpe@users.noreply.github.com>
2026-01-22 13:15:02 +0100

    more endorsements changes

--- projects/demo/models/staging/decidim/stg_decidim_endorsements.sql
+++ projects/demo/models/staging/decidim/stg_decidim_endorsements.sql
@@ -1,3 +1,10 @@
+{% set relation = adapter.get_relation(
+    database=target.database,
+    schema='public',
+    identifier='decidim_likes'
+) %}
+
+{% if relation is not none %}
 SELECT
     id,
     resource_type,
@@ -7,5 +14,18 @@ SELECT
     decidim_user_group_id,
     created_at,
     updated_at
+FROM {{ source('decidim', 'decidim_likes') }}
+
+{% else %}
+ SELECT
+    id,
+    resource_type,
+    resource_id,
+    decidim_author_type,
+    decidim_author_id,
+    decidim_user_group_id,
+    created_at,
+    updated_at
 FROM {{ source('decidim', 'decidim_endorsements') }}
-  
+
+{% endif %}
