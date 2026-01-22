a52bca599d4e85d309bc6dc1cf9471145ed429f6
ailepet <hellpe@users.noreply.github.com>
2026-01-22 16:02:31 +0100

    filter out deleted debates and blog posts of downstream models

--- projects/demo/models/marts/blogs_posts/blogs_posts.sql
+++ projects/demo/models/marts/blogs_posts/blogs_posts.sql
@@ -9,3 +9,4 @@ SELECT
     concat('https://', decidim_components.organization_host, '/', decidim_components.ps_space_type_slug, '/', decidim_components.ps_slug, '/f/', decidim_components.id, '/posts/', decidim_blogs_posts.id) AS post_url
 FROM {{ ref ("stg_decidim_blogs_posts")}} AS decidim_blogs_posts
 JOIN {{ ref ("components")}} AS decidim_components ON decidim_components.id = decidim_component_id
+WHERE decidim_blogs_posts.deleted_at IS NULL
