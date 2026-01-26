3abea136e23a9d139d4a51440a541cfc1b90fdf7
ailepet <hellpe@users.noreply.github.com>
2026-01-26 16:49:40 +0100

    add taxonomies macros

---
+++ projects/demo/macros/taxonomization_category.sql
@@ -0,0 +1,25 @@
+{% macro taxonomization_first_category(column_name) %}
+    {% set lang = var('DBT_LANG', 'fr') %}  -- Default language is 'fr'
+
+    {% set translations = {
+        'fr': "Sans catégorie",
+        'en': "No category",
+        'de': "Keine Kategorie",
+        'nl': "Geen categorie"
+    } %}
+
+    COALESCE(NULLIF({{ column_name }}, ''), '{{ translations.get(lang, "Unknown") }}') AS first_category
+{% endmacro %}
+
+{% macro taxonomization_first_sub_category(column_name) %}
+    {% set lang = var('DBT_LANG', 'fr') %}  -- Default language is 'fr'
+
+    {% set translations = {
+        'fr': "Sans sous-catégorie",
+        'en': "No subcategory",
+        'de': "Keine Unterkategorie",
+        'nl': "Geen subcategorie"
+    } %}
+
+    COALESCE(NULLIF({{ column_name }}, ''), '{{ translations.get(lang, "Unknown") }}') AS first_sub_category
+{% endmacro %}
