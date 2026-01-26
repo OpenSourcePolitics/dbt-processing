401327e3db0f305fa187883824fb8c56e8b6079d
ailepet <hellpe@users.noreply.github.com>
2026-01-26 15:24:53 +0100

    fix typo

--- projects/demo/models/staging/decidim/stg_decidim_surveys.sql
+++ projects/demo/models/staging/decidim/stg_decidim_surveys.sql
@@ -7,7 +7,7 @@ renamed as (
         decidim_component_id,
         created_at,
         updated_at,
-        {{ get_column_if_exists(source('decidim', 'decidim_blogs_posts'), 'deleted_at', 'TIMESTAMP') }}
+        {{ get_column_if_exists(source('decidim', 'decidim_surveys_surveys'), 'deleted_at', 'TIMESTAMP') }}
     FROM source
 )
 SELECT * FROM renamed
