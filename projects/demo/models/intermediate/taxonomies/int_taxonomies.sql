552676766b23b63e63e4841a22619d0874dbc9c6
ailepet <hellpe@users.noreply.github.com>
2026-02-18 16:32:09 +0100

    fix cast type for pre-031 instances

--- projects/demo/models/intermediate/taxonomies/int_taxonomies.sql
+++ projects/demo/models/intermediate/taxonomies/int_taxonomies.sql
@@ -10,6 +10,7 @@ SELECT
     decidim_taxonomies.updated_at,
     decidim_taxonomies.filters_count,
     decidim_taxonomies.filter_items_count,
-    decidim_taxonomies.part_of
+    decidim_taxonomies.part_of,
+    (CASE WHEN JSON_EXISTS(decidim_taxonomies.part_of, '1') THEN true ELSE false END) AS is_scope
 FROM {{ ref ("stg_decidim_taxonomies") }}  as decidim_taxonomies
 JOIN {{ ref ("int_organizations")}}  AS decidim_organizations ON decidim_organization_id = decidim_organizations.id
--- projects/demo/models/staging/decidim/taxonomies/stg_decidim_taxonomies.sql
+++ projects/demo/models/staging/decidim/taxonomies/stg_decidim_taxonomies.sql
@@ -23,17 +23,17 @@
     FROM {{ source('decidim', 'decidim_taxonomies') }}
 {% else %}
     SELECT
-        CAST(NULL AS INTEGER) AS id,
+        CAST(NULL AS BIGINT) AS id,
         CAST(NULL AS TEXT) AS name,
-        CAST(NULL AS INTEGER) AS decidim_organization_id,
-        CAST(NULL AS INTEGER) AS parent_id,
-        CAST(NULL AS INTEGER) AS weight,
-        CAST(NULL AS INTEGER) AS children_count,
-        CAST(NULL AS INTEGER) AS taxonomizations_count,
+        CAST(NULL AS BIGINT) AS decidim_organization_id,
+        CAST(NULL AS BIGINT) AS parent_id,
+        CAST(NULL AS BIGINT) AS weight,
+        CAST(NULL AS BIGINT) AS children_count,
+        CAST(NULL AS BIGINT) AS taxonomizations_count,
         CAST(NULL AS TIMESTAMP) AS created_at,
         CAST(NULL AS TIMESTAMP) AS updated_at,
-        CAST(NULL AS INTEGER) AS filters_count,
-        CAST(NULL AS INTEGER) AS filter_items_count,
+        CAST(NULL AS BIGINT) AS filters_count,
+        CAST(NULL AS BIGINT) AS filter_items_count,
         CAST(NULL AS JSONB) AS part_of
     LIMIT 0
 {% endif %}
