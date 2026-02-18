8122763cd2a077d71d675e254dbfcd7eea74b125
ailepet <hellpe@users.noreply.github.com>
2026-02-18 16:52:41 +0100

    fix cast type for pre-031 instances

--- projects/demo/models/intermediate/taxonomies/int_taxonomies.sql
+++ projects/demo/models/intermediate/taxonomies/int_taxonomies.sql
@@ -11,6 +11,9 @@ SELECT
     decidim_taxonomies.filters_count,
     decidim_taxonomies.filter_items_count,
     decidim_taxonomies.part_of,
-    (CASE WHEN JSON_EXISTS(decidim_taxonomies.part_of, '1') THEN true ELSE false END) AS is_scope
+    EXISTS (
+        SELECT 1 FROM jsonb_array_elements(decidim_taxonomies.part_of) AS elem
+        WHERE elem::text = '1'
+    ) AS is_scope
 FROM {{ ref ("stg_decidim_taxonomies") }}  as decidim_taxonomies
 JOIN {{ ref ("int_organizations")}}  AS decidim_organizations ON decidim_organization_id = decidim_organizations.id
--- projects/demo/models/staging/decidim/taxonomies/stg_decidim_taxonomy_filters.sql
+++ projects/demo/models/staging/decidim/taxonomies/stg_decidim_taxonomy_filters.sql
@@ -20,12 +20,12 @@
     FROM {{ source('decidim', 'decidim_taxonomy_filters') }}
 {% else %}
     SELECT
-        CAST(NULL AS INTEGER) AS id,
-        CAST(NULL AS INTEGER) AS root_taxonomy_id,
-        CAST(NULL AS INTEGER) AS filter_items_count,
+        CAST(NULL AS BIGINT) AS id,
+        CAST(NULL AS BIGINT) AS root_taxonomy_id,
+        CAST(NULL AS BIGINT) AS filter_items_count,
         CAST(NULL AS TIMESTAMP) AS created_at,
         CAST(NULL AS TIMESTAMP) AS updated_at,
-        CAST(NULL AS INTEGER) AS components_count,
+        CAST(NULL AS BIGINT) AS components_count,
         CAST(NULL AS TEXT) AS name,
         CAST(NULL AS TEXT) AS internal_name,
         CAST(NULL AS JSONB) AS participatory_space_manifests
