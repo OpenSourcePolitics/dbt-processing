aeef73f63ec8031ba3db3efba2a0807ab73af0e1
ailepet <hellpe@users.noreply.github.com>
2026-02-23 10:33:40 +0100

    fix first categoruy/subcategory columns

--- projects/demo/models/marts/proposals/all_proposals.sql
+++ projects/demo/models/marts/proposals/all_proposals.sql
@@ -20,7 +20,7 @@ taxonomizations AS (
 scopes AS (
     {{ import_scopes_from_taxonomies('Decidim::Proposals::Proposal') }}
 ),
-categories AS (
+categories_from_taxonomies AS (
     {{ import_categories_from_taxonomies('Decidim::Proposals::Proposal') }}
 ),
 categorizations AS (
@@ -57,20 +57,16 @@ proposals AS (
         coauthorships.authors_ids,
         COALESCE(coauthorships.authors_ids[1], -1) AS first_author_id,
         decidim_proposals.address,
-        (CASE WHEN categories.is_category
-            THEN
-            categories.categories
-            ELSE
-            categorizations.categories
-        END) AS categories,
-        {{ categorization_first_category('categorizations.categories[1]') }},
-        (CASE WHEN categories.is_category
-            THEN
-            categories.sub_categories
-            ELSE
-            categorizations.sub_categories
-        END) AS sub_categories,
-        {{ categorization_first_sub_category('categorizations.sub_categories[1]') }},
+        COALESCE(categories_from_taxonomies.categories, categorizations.categories) AS categories,
+        COALESCE(
+            {{ categorization_first_category('categories_from_taxonomies.categories[1]') }},
+            {{ categorization_first_category('categorizations.categories[1]') }}
+        ) AS first_category,
+        COALESCE(categories_from_taxonomies.sub_categories, categorizations.sub_categories) AS sub_categories,
+        COALESCE(
+            {{ categorization_first_sub_category('categories_from_taxonomies.sub_categories[1]') }},
+            {{ categorization_first_sub_category('categorizations.sub_categories[1]') }}
+        ) AS first_sub_category,
         taxonomizations.taxonomies,
         {{ taxonomization_first_taxonomy('taxonomizations.taxonomies[1]') }},
         taxonomizations.sub_taxonomies,
@@ -92,7 +88,7 @@ proposals AS (
     LEFT JOIN votes ON decidim_proposals.id = votes.decidim_proposal_id
     LEFT JOIN taxonomizations on taxonomizations.taxonomizable_id = decidim_proposals.id
     LEFT JOIN scopes on scopes.taxonomizable_id = decidim_proposals.id
-    LEFT JOIN categories on categories.taxonomizable_id = decidim_proposals.id
+    LEFT JOIN categories_from_taxonomies on categories_from_taxonomies.taxonomizable_id = decidim_proposals.id
     LEFT JOIN categorizations ON categorizations.categorizable_id = decidim_proposals.id
     LEFT JOIN {{ ref("int_scopes")}} AS decidim_scopes ON decidim_scopes.id = decidim_proposals.decidim_scope_id
     LEFT JOIN {{ ref("stg_decidim_proposals_custom_states")}} AS decidim_proposals_proposal_states ON decidim_proposals_proposal_states.id = decidim_proposals.decidim_proposals_proposal_state_id
